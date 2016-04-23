975c479e1fa62b73024acadab555092597fa086c
Raghu Betina <raghubetina@users.noreply.github.com>
2016-04-23 09:36:56 -0500

    Include explicit render statements

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -5,9 +5,13 @@ class FortunesController < ApplicationController
     @third_number = rand(100)
     @fourth_number = rand(100)
     @fifth_number = rand(100)
+
+    render("lucky.html.erb")
   end
 
   def unlucky
     @numbers = [rand(100...1000), rand(100...1000), rand(100...1000), rand(100...1000), rand(100...1000)]
+
+    render("unlucky.html.erb")
   end
 end
